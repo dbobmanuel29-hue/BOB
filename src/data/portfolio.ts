92aaf8f6a8b4ce4fddf976717310291a7ffc09d5
BOBMANUEL TAMUNODIEPRIYE SOGBEYE <dbobmanuel29@gmail.com>
2026-09-22 10:52:52 +0100

    Add src/data/portfolio.ts

---
+++ src/data/portfolio.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    platform: "TikTok",
+    handle: "@itsglitchronin.vfx",
+    url: "https://www.tiktok.com/@itsglitchronin.vfx",
+  },
+  {
+    platform: "Instagram",
+    handle: "@scott_vfx",
+    url: "https://www.instagram.com/scott_vfx",
+  },
+  {
+    platform: "YouTube",
+    handle: "@scottedits-f2g",
+    url: "https://www.youtube.com/@scottedits-f2g",
+  },
+] as const;
